68c4b83306598fac9938b1509583034559c7f968
RioNagata <109705320+RioNagata@users.noreply.github.com>
2022-10-12 16:44:14 +1000

    small changes to add user

--- src/app/adduser/adduser.component.ts
+++ src/app/adduser/adduser.component.ts
@@ -39,29 +39,37 @@ export class AdduserComponent implements OnInit {
     if(this.userid == null){
       this.iderrorshow = !this.iderrorshow
     } else {
-      this.newuser = {
-        // sets newuser array value using the data from user
-        username: this.username,
-        email: this.email,
-        userid: this.userid,
-        password: this.password,
-        userrole: this.userrole
-    }
-    // sends the data to the server-side, resets all data
-      this.userdata.add(this.newuser!).subscribe((data: any)=>{
-        this.noticeshow;
-        if(data.err == null){
-          this.newProductMessage = data.num + " new user (" + this.username + ") was added";
-        } else {
-          this.newProductMessage = data.err;
+      if(this.username == "" || this.userid == "" || this.password == "" || this.userrole == "" || this.email == ""){
+        alert('missing information');
+      } else {
+        if(this.userrole == 'user' || this.userrole == 'gassist' || this.userrole == 'gadmin' || this.userrole == 'super' ){
+          this.newuser = {
+            // sets newuser array value using the data from user
+            username: this.username,
+            email: this.email,
+            userid: this.userid,
+            password: this.password,
+            userrole: this.userrole
         }
-        this.userid = "";
-        this.username = "";
-        this.email = "";
-        this.userrole = "";
-        this.password = "";
-        this.router.navigateByUrl('');
-      });
+        // sends the data to the server-side, resets all data
+          this.userdata.add(this.newuser!).subscribe((data: any)=>{
+            this.noticeshow;
+            if(data.err == null){
+              this.newProductMessage = data.num + " new user (" + this.username + ") was added";
+            } else {
+              this.newProductMessage = data.err;
+            }
+            this.userid = "";
+            this.username = "";
+            this.email = "";
+            this.userrole = "";
+            this.password = "";
+            this.router.navigateByUrl('user');
+          });
+        } else {
+          alert('user role needs to be super, gassist, gadmin or user');
+        }
+      }
     }
   }
 }
